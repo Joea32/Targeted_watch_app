5bfbb0ed55a2f887c928399227cdcc916fd30696
Joea32 <josephattuah1999@gmail.com>
2025-06-11 01:21:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,7 @@ from utils import (
 load_dotenv()
 
 # ------------------ App Initialization ------------------
-
-from flask import Flask
-from flask_login import LoginManager
-
 app = Flask(__name__)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 # Load configuration depending on environment
 env = os.environ.get('FLASK_ENV', 'development')
@@ -85,7 +78,9 @@ ALLOWED_EXTENSIONS = {
 }
 
 # ------------------ Init Extensions ------------------
-from flask_login import LoginManager
+#from flask_login import LoginManager
+
+from extensions import db, migrate, mail, login_manager
 
 #db.init_app(app)
 #migrate.init_app(app, db)
@@ -93,6 +88,12 @@ from flask_login import LoginManager
 #login_manager = LoginManager(app)
 #login_manager.init_app(app)
 #login_manager.login_view = 'login'
+
+db.init_app(app)
+migrate.init_app(app, db)
+mail.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'  # adjust if your login view is different
 
 # ------------------ Flask-Login User Loader ------------------
 @login_manager.user_loader
