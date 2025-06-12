f0df2c6db43ea4a59e805d091a58bf9a2cdc3420
Joea32 <josephattuah1999@gmail.com>
2025-06-12 03:44:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1141,13 +1141,6 @@ def login_test():
     </form>
     '''
 
-@app.errorhandler(403)
-def forbidden(e):
-    return render_template('403.html'), 403
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 @app.route('/confirm/<token>')
 def confirm_email(token):
